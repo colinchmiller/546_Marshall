764477132b5f60676da173f4721418cd675dc041
colinmiller <colinchmiller@gmail.com>
2016-02-25 11:53:52 -0600

    basic test success angular app and congroller

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,6 @@
-console.log("testing");
+var mainApp = angular.module('mainApp', []);
+
+mainApp.controller('testController', ['$scope', function($scope){
+  console.log("testController firing!");
+  $scope.test = "This is a test section!";
+}]);
