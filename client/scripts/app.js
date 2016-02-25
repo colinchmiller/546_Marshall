28200ca4342afc4114dc39b5e9bb1934db40f5d4
colinmiller <colinchmiller@gmail.com>
2016-02-25 12:40:15 -0600

    add and successful test home, history, architect, tour, and contact routes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,9 +2,25 @@ var mainApp = angular.module('mainApp', ['ngRoute']);
 
 mainApp.config(['$routeProvider', function($routeProvider){
       $routeProvider.
+          when('/home', {
+            templateUrl: 'assets/views/routes/home.html',
+            controller: 'HomeController'
+          }).
           when('/history', {
             templateUrl: 'assets/views/routes/history.html',
             controller: 'HistoryController'
+          }).
+          when('/architect', {
+            templateUrl: 'assets/views/routes/architect.html',
+            controller: 'ArchitectController'
+          }).
+          when('/tour', {
+            templateUrl: 'assets/views/routes/tour.html',
+            controller: 'TourController'
+          }).
+          when('/contact', {
+            templateUrl: 'assets/views/routes/contact.html',
+            controller: 'ContactController'
           })
 }]);
 
@@ -13,7 +29,27 @@ mainApp.controller('testController', ['$scope', function($scope){
   $scope.test = "This is a test section!";
 }]);
 
+mainApp.controller('HomeController', ['$scope', function($scope){
+  console.log("HomeController firing!");
+  $scope.test = "This is the Home Page";
+}]);
+
 mainApp.controller('HistoryController', ['$scope', function($scope){
   console.log("HistoryController firing!");
-  $scope.test = "This is a test of the history route on the DOM";
-}])
+  $scope.test = "This is the History Page";
+}]);
+
+mainApp.controller('ArchitectController', ['$scope', function($scope){
+  console.log("ArchitectController firing!");
+  $scope.test = "This is the Architect Page";
+}]);
+
+mainApp.controller('TourController', ['$scope', function($scope){
+  console.log("TourController is firing!");
+  $scope.test = "This is the Tour Page";
+}]);
+
+mainApp.controller('ContactController', ['$scope', function($scope){
+  console.log("ContactController is firing!");
+  $scope.test = "This is the Contact Page"
+}]);
