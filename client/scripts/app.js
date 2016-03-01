21d85d040a2b5114b827ebc39fceafec814addfd
colinmiller <colinchmiller@gmail.com>
2016-03-01 12:14:45 -0600

    controller cleanup, removal excess scope coding

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -29,25 +29,20 @@ mainApp.config(['$routeProvider', function($routeProvider){
 
 mainApp.controller('HomeController', ['$scope', function($scope){
   console.log("HomeController firing!");
-  $scope.test = "This is the Home Page";
 }]);
 
 mainApp.controller('HistoryController', ['$scope', function($scope){
   console.log("HistoryController firing!");
-  $scope.test = "This is the History Page";
 }]);
 
 mainApp.controller('ArchitectController', ['$scope', function($scope){
   console.log("ArchitectController firing!");
-  $scope.test = "This is the Architect Page";
 }]);
 
 mainApp.controller('TourController', ['$scope', function($scope){
   console.log("TourController is firing!");
-  $scope.test = "This is the Tour Page";
 }]);
 
 mainApp.controller('ContactController', ['$scope', function($scope){
   console.log("ContactController is firing!");
-  $scope.test = "This is the Contact Page"
 }]);
